b6123412bc9d1b1bd7de47d5618a26de5fcc5aa4
Jared Moore <jaredm@ca.ibm.com>
2020-09-02 11:01:03 -0400

    Add typehinting and error checking.

--- lib/ibm/cloud/sdk/vpc/base_vpc.rb
+++ lib/ibm/cloud/sdk/vpc/base_vpc.rb
@@ -1,54 +1,85 @@
 # frozen_string_literal: true
+# typed: strict
+
+require 'sorbet-runtime'
+require('helpers/response')
 
 module IBM
   module Cloud
     module SDK
       # Container that encapsulates the VPC API.
-      class BaseVPC
+      class BaseVPC < BaseService
+        extend T::Sig
+
+        sig { params(method: String, path: T.nilable(String), params: T.nilable(T.untyped)).returns(RestClient::Response) }
+        def adhoc(method = 'get', path = nil, params = nil)
+          RestClient::Request.execute(method: method.to_sym, url: url(path), headers: metadata(params))
+        rescue RestClient::ExceptionWithResponse => e
+          @logger.error("Exception for #{e.response.request.url} #{e.response.description}")
+          e.response
+        end
+
+        sig { returns(String) }
+        attr_reader :endpoint
+
+        private
+
+        sig { params(endpoint: String, token: IBM::Cloud::SDK::IAM::Token, logger: Log).void }
         def initialize(endpoint, token, logger)
-          @endpoint = endpoint
-          @token = token
-          @logger = logger.get_logger(self.class) unless logger.nil?
+          @endpoint = T.let(endpoint, String)
+          @token = T.let(token, IBM::Cloud::SDK::IAM::Token)
+          @logger = T.let(logger.get_logger(self.class), Log) unless logger.nil?
         end
 
-        def get(path, params = nil)
+        sig { params(path: T.nilable(String), params: T.nilable(T::Hash[String, String])).returns(RestClient::Response) }
+        def get(path = nil, params = nil)
           begin
-            response = RestClient.get(url(path), metadata(params))
+            response = T.let(RestClient.get(url(path), metadata(params)), RestClient::Response)
           rescue RestClient::ExceptionWithResponse => e
-            @logger.error("Exception for #{e.response.request.url} #{e.response.description}")
-            return e.response
+            @logger.error("Exception for #{e.response.request.url} #s{e.response.description}")
+            response = T.let(e.response, RestClient::Response)
           end
-          JSON.parse(response)
+          response
         end
 
-        def post(path, payload, params = nil)
-          response = RestClient.post(url(path), payload, metadata(params))
-          JSON.parse(response)
+        sig { params(payload: T::Hash[String, String], path: T.nilable(String), params: T.nilable(T::Hash[String, String])).returns(RestClient::Response) }
+        def post(payload, path = nil, params = nil)
+          RestClient.post(url(path), payload, metadata(params))
+        rescue RestClient::ExceptionWithResponse => e
+          @logger.error("Exception for #{e.response.request.url} #{e.response.description}")
+          e.response
         end
 
-        def put(path, payload, params = nil)
-          response = RestClient.put(url(path), payload, metadata(params))
-          JSON.parse(response)
+        sig { params(payload: T::Hash[String, String], path: T.nilable(String), params: T.nilable(T::Hash[String, String])).returns(RestClient::Response) }
+        def put(payload, path = nil, params = nil)
+          RestClient.put(url(path), payload, metadata(params))
+        rescue RestClient::ExceptionWithResponse => e
+          @logger.error("Exception for #{e.response.request.url} #{e.response.description}")
+          e.response
         end
 
-        def patch(path, payload, params = nil)
-          response = RestClient.patch(url(path), payload, metadata(params))
-          JSON.parse(response)
+        sig { params(payload: T::Hash[String, String], path: T.nilable(String), params: T.nilable(T::Hash[String, String])).returns(RestClient::Response) }
+        def patch(payload, path = nil, params = nil)
+          RestClient.patch(url(path), payload, metadata(params))
+        rescue RestClient::ExceptionWithResponse => e
+          @logger.error("Exception for #{e.response.request.url} #{e.response.description}")
+          e.response
         end
 
-        def delete(path, params = nil)
+        sig { params(path: T.nilable(String), params: T.nilable(T::Hash[String, String])).returns(RestClient::Response) }
+        def delete(path = nil, params = nil)
           response = RestClient.delete(url(path), metadata(params))
           JSON.parse(response)
         end
 
-        attr_reader :endpoint
-
-        def url(path)
+        sig { params(path: T.nilable(String)).returns(String) }
+        def url(path = nil)
           return endpoint if path.nil?
 
           "#{endpoint}/#{path}"
         end
 
+        sig { returns(T::Hash[String, String]) }
         def headers
           head = {
             'Authorization' => @token.authorization_header
@@ -56,15 +87,18 @@ module IBM
           head
         end
 
+        sig { params(params: T.untyped).returns(T::Hash[Symbol, T.any(String, Integer)]) }
         def metadata(params)
           { params: merge_params(params) }.merge(headers)
         end
 
+        sig { params(params: T.nilable(T::Hash[Symbol, T.untyped])).returns(T::Hash[Symbol, T.untyped]) }
         def merge_params(params)
           params.nil? && params = {}
           version_param.merge(params)
         end
 
+        sig { returns(T::Hash[Symbol, T.any(String, Integer)]) }
         def version_param
           { version: '2020-08-01', generation: 2 }
         end
