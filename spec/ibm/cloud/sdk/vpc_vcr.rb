a6517e63c30cb093f9e59d95ac1cf8add853e3c7
Jared Moore <jaredm@ca.ibm.com>
2020-09-25 12:02:57 -0400

    Change the token expiration date to be 2030.

--- spec/ibm/cloud/sdk/vpc_vcr.rb
+++ spec/ibm/cloud/sdk/vpc_vcr.rb
@@ -9,7 +9,7 @@ ENV['IBM_CLOUD_APIKEY'] = 'API_KEY' unless ENV['IBM_CLOUD_APIKEY']
 # Iterate through the SSH keys and replace all values.
 # Being very careful with this one as fixing a data leak would be costly.
 def replace_ssh_keys(response)
-  data = JSON.parse(response.body, { symbolize_names: true })
+  data = JSON.parse(response.body, symbolize_names: true)
   raise 'The keys key is not found in the json response.' unless data.key?(:keys)
 
   keys = { fingerprint: 'SHA256:xxxxxxx', public_key: 'RSA: VVVVVV' }
@@ -17,7 +17,13 @@ def replace_ssh_keys(response)
     v.merge!(keys)
     v[:name] = "random_key_#{i}"
   end
-  response.body = data.to_json
+  response.body = data.to_json.force_encoding('ASCII-8BIT')
+end
+
+def replace_token_contents(response)
+  data = JSON.parse(response.body, symbolize_names: true)
+  data.merge!({ refresh_token: '11111', ims_user_id: '22222', expiration: 1893456000 })
+  response.body = data.to_json.force_encoding('ASCII-8BIT')
 end
 
 VCR.configure do |c|
@@ -26,17 +32,20 @@ VCR.configure do |c|
   c.define_cassette_placeholder('API_KEY') { ENV['IBM_CLOUD_APIKEY'] }
   c.allow_http_connections_when_no_cassette = false
   c.configure_rspec_metadata!
-
-  # Don't record non 200 responses.
   c.before_record do |interaction|
-    interaction.ignore! if interaction.response.status.code != 200
+    # Don't save results if results are not in 200-series.
+    interaction.ignore! if interaction.response.status.code > 200 || interaction.response.status.code >= 300
     interaction.request.headers['Authorization'] = 'Bearer xxxxxx' if interaction.request.headers.key?('Authorization')
 
     # Clean up token save.
     if interaction.request.uri.end_with?('identity/token')
-      # We might might want to use a hash here. But this works for the time being.
-      interaction.response.body.gsub!(/"refresh_token":"[\w\-]{1,}/, '"refresh_token":"11111')
-      interaction.response.body.gsub!(/"ims_user_id":\d{1,}/, '"ims_user_id":1212')
+      begin
+        replace_token_contents(interaction.response)
+      rescue => e # rubocop:disable Style/RescueStandardError # Don't know what errors are here so catch them all.
+        # If there is an exception during fetching we don't want to save the results.
+        puts("Exception raised during fetching of token #{e}")
+        interaction.ignore!
+      end
     end
 
     # Replace IP V4 Addresses
