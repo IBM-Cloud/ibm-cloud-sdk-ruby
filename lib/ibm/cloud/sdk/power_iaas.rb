09724d737894e7c18319d94882a9c89d5b5a38c9
Jay Carman <jwcarman@us.ibm.com>
2020-09-26 07:50:04 -0500

    Add and update PowerIaas doc strings

--- lib/ibm/cloud/sdk/power_iaas.rb
+++ lib/ibm/cloud/sdk/power_iaas.rb
@@ -17,6 +17,9 @@ module IBM
           @tenant = tenant
         end
 
+        # Get the API service Endpoint URL
+        #
+        # @return [String] Endpoint URL
         def endpoint
           "https://#{region.sub(/-\d$/, '')}.power-iaas.cloud.ibm.com/pcloud/v1"
         end
@@ -41,12 +44,15 @@ module IBM
 
         # Get an IBM Power Cloud PVM instance
         #
-        # @param pvm_instance_id [String] the PVM instance ID
+        # @param pvm_instance_id [String] PVM instance ID
         # @return [Hash] PVM Instances
         def get_pvm_instance(instance_id)
           get("cloud-instances/#{guid}/pvm-instances/#{instance_id}")
         end
 
+        # Perform 'start' action on a PVMInstance
+        #
+        # @param pvm_instance_id [String] PVM instance ID
         def start_pvm_instance(instance_id)
           post(
             "cloud-instances/#{guid}/pvm-instances/#{instance_id}/action",
@@ -54,6 +60,9 @@ module IBM
           )
         end
 
+        # Perform 'stop' action on a PVMInstance
+        #
+        # @param pvm_instance_id [String] PVM instance ID
         def stop_pvm_instance(instance_id)
           post(
             "cloud-instances/#{guid}/pvm-instances/#{instance_id}/action",
@@ -61,6 +70,12 @@ module IBM
           )
         end
 
+        # Perform 'reboot' action on a PVMInstance
+        #
+        # This action has been deprecated and replaced by 'soft-reboot'
+        # and 'hard-reboot'
+        #
+        # @param pvm_instance_id [String] PVM instance ID
         def reboot_pvm_instance(instance_id)
           post(
             "cloud-instances/#{guid}/pvm-instances/#{instance_id}/action",
@@ -68,10 +83,17 @@ module IBM
           )
         end
 
+        # Create a new PVM instance
+        #
+        # @param instance_hash [Hash] New instance attributes
+        # @return [Hash] PVMInstance
         def create_pvm_instance(instance_hash)
           post("cloud-instances/#{guid}/pvm-instances", instance_hash.to_json)
         end
 
+        # Delete a PVM instance
+        #
+        # @param pvm_instance_id [String] PVM instance ID
         def delete_pvm_instance(instance_id)
           delete("cloud-instances/#{guid}/pvm-instances/#{instance_id}")
         end
@@ -89,7 +111,7 @@ module IBM
 
         # Get an IBM Power Cloud image
         #
-        # @param image_id [String] The ID of an Image
+        # @param image_id [String] Image ID
         # @return [Hash] Image
         def get_image(image_id)
           get("cloud-instances/#{guid}/images/#{image_id}")
@@ -97,6 +119,9 @@ module IBM
           nil
         end
 
+        # Delete an image from a cloud instance
+        #
+        # @param image_id [String] Image ID
         def delete_image(image_id)
           delete("cloud-instances/#{guid}/images/#{image_id}")
         end
@@ -114,7 +139,7 @@ module IBM
 
         # Get a specific volume
         #
-        # @param volume_id [String] The ID of a volume
+        # @param volume_id [String] Image ID
         # @return [Hash] Volume
         def get_volume(volume_id)
           get("cloud-instances/#{guid}/volumes/#{volume_id}")
@@ -122,7 +147,7 @@ module IBM
 
         # Delete a volume
         #
-        # @param volume_id [String] The ID of a volume
+        # @param volume_id [String] Volume ID
         def delete_volume(volume_id)
           delete("cloud-instances/#{guid}/volumes/#{volume_id}")
         end
@@ -147,7 +172,7 @@ module IBM
 
         # Get an IBM Power Cloud network
         #
-        # @param network_id [String] the network ID
+        # @param network_id [String] Network ID
         # @return [Hash] Network
         def get_network(network_id)
           get("cloud-instances/#{guid}/networks/#{network_id}")
@@ -161,42 +186,64 @@ module IBM
         end
 
         # Get a storage types list in IBM  Power Cloud.
-        # note: this mehod to be refactored under the common 
+        # note: this method to be refactored under the common
         # IBM::Cloud::SDK::PowerIaas.endpoint when the rest api become available.
         #
-        # @return [Hash] StorageType
+        # @return [Array<Hash>] StorageType
         def get_storage_types
           JSON.parse(RestClient.get("https://#{region.sub(/-\d$/, '')}.power-iaas.cloud.ibm.com/broker/v1/storage-types", headers))[region]
         end
 
+        # Create a new network
+        #
+        # @param network_hash [Hash] New network attributes
+        # @return [Hash] Network
         def create_network(network_hash)
           post("cloud-instances/#{guid}/networks", network_hash.to_json)
         end
 
+        # Delete a network
+        #
+        # @param network_id [String] Network ID
         def delete_network(network_id)
           delete("cloud-instances/#{guid}/networks/#{network_id}")
         end
 
+        # Get all ports for a network
+        #
+        # @param network_id [String] Network ID
+        # @return [Array<Hash>] NetworkPort
         def get_network_ports(network_id)
           get("cloud-instances/#{guid}/networks/#{network_id}/ports")["ports"]
         end
 
+        # List the tenant's SSH Keys
+        #
+        # @return [Array<Hash>] SSHKey
         def get_ssh_keys
           get("tenants/#{tenant}")["sshKeys"]
         end
 
+        # Add a new SSH key to the tenant
+        #
+        # @param name [String] User defined name for the SSH key
+        # @param sshkey [String] SSH RSA key
+        # @return [Hash] SSHKey
         def create_key_pair(name, sshkey)
           payload = {"name" => name, "sshkey" => sshkey}
           post("tenants/#{tenant}/sshkeys", payload.to_json)
         end
 
+        # Delete SSH key from the tenant
+        #
+        # @param sshkey_name [String] SSH Key name
         def delete_key_pair(name)
           delete("tenants/#{tenant}/sshkeys/#{name}")
         end
 
         # Get an SAP profile
         #
-        # @param sap_profile_id [String] The ID of an SAP profile
+        # @param sap_profile_id [String] SAP Profile ID
         # @return [Hash] SAP profile
         def get_sap_profile(sap_profile_id)
           get("cloud-instances/#{guid}/sap/#{sap_profile_id}")
@@ -219,7 +266,7 @@ module IBM
         # instance is the standard 'PVMInstance' type and can be accessed via
         # the existing *_pvm_instance methods.
         #
-        # @param sap_profile_id [String] The ID of an SAP profile
+        # @param sap_profile_id [String] SAP Profile ID
         # @return [Hash] SAP profile
         def create_sap_pvm_instance(instance_hash)
           post("cloud-instances/#{guid}/sap", instance_hash.to_json)
