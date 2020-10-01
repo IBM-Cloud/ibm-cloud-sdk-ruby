dd6d1bf54706a62b7ca63e61051aef8721070a69
Jared Moore <jaredm@ca.ibm.com>
2020-10-01 19:48:25 -0400

    Remove spurious methods.

--- lib/ibm/cloud/sdk/vpc/keys.rb
+++ lib/ibm/cloud/sdk/vpc/keys.rb
@@ -10,28 +10,6 @@ module IBM
           def initialize(parent)
             super(parent, 'keys', child_class: Key)
           end
-
-          def fetch(resource_id: nil)
-            params = {}
-            params['resource_group.id'] = resource_id if resource_id
-            get(params: params)
-          end
-
-          def all(resource_id = nil)
-            fetch(resource_id: resource_id).subkey(@array_key)
-          end
-
-          # :reek:FeatureEnvy
-          def create(name, public_key, resource_group: nil, type: nil)
-            payload = { name: name, public_key: public_key }
-            payload[:resource_group] = resource_group if resource_group
-            payload[:type] = type if type
-            post(payload)
-          end
-
-          def key(id)
-            Key.new(self, id)
-          end
         end
 
         # A single key.
