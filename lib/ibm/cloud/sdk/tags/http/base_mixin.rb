4974aa94afac25a8d7ac7f73a01a042f59ab8ca3
Jared Moore <jaredm@ca.ibm.com>
2020-10-07 09:10:11 -0400

    Add tags http subclasses

---
+++ lib/ibm/cloud/sdk/tags/http/base_mixin.rb
@@ -0,0 +1,18 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative '../http'
+
+module IBM
+  module Cloud
+    module SDK
+      module TAG
+        module HTTP
+          module BaseMixin
+            include SDKHTTP::BaseHTTPMixin
+          end
+        end
+      end
+    end
+  end
+end
