9bbe675fe95bfd7eb6b7ac0c142404b8fa8292a0
Jared Moore <jaredm@ca.ibm.com>
2020-10-07 16:42:16 -0400

    Add tags to BaseInstance

--- lib/ibm/cloud/sdk/vpc/http_vpc/vpc_instance.rb
+++ lib/ibm/cloud/sdk/vpc/http_vpc/vpc_instance.rb
@@ -1,15 +1,28 @@
 # frozen_string_literal: true
 
+require 'ibm/cloud/sdk/tags'
+
 require_relative 'vpc_http'
 
 module IBM
   module Cloud
     module SDK
-      # Module holds basic HTTP functionality.
       module VPC
         # Container that encapsulates the VPC API.
         class BaseInstance < SDKHTTP::BaseInstance
           include VpcHTTP
+
+          # Get the CRN for the resource.
+          # @return [String]
+          def crn
+            self[:crn]
+          end
+
+          # Get the tags associated with this resource.
+          # @return [Enumerator] An enumerator with the results.
+          def tags
+            IBM::Cloud::SDK::Tags.new(@connection, @token, logger: @logger).attached_to(crn)
+          end
         end
       end
     end
