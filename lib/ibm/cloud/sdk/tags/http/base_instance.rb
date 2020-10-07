06ea890c0760a1fdbf43acb7b71b817f1527fbc1
Jared Moore <jaredm@ca.ibm.com>
2020-10-07 09:10:11 -0400

    Add tags http subclasses

---
+++ lib/ibm/cloud/sdk/tags/http/base_instance.rb
@@ -0,0 +1,17 @@
+# typed: true
+# frozen_string_literal: true
+
+require_relative '../http'
+
+module IBM
+  module Cloud
+    module SDK
+      module TAG
+        module HTTP
+          class BaseInstance < SDKHTTP::BaseInstance
+          end
+        end
+      end
+    end
+  end
+end
