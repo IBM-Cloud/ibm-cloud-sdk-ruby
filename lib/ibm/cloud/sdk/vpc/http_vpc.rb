f209c39345c0e8981d79fa2c462cb9804d484409
Jared Moore <jaredm@ca.ibm.com>
2020-10-08 11:00:10 -0400

    Rename vpc_http to http

--- lib/ibm/cloud/sdk/vpc/http_vpc.rb
+++ lib/ibm/cloud/sdk/vpc/http_vpc.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 require 'ibm/cloud/sdk_http'
-require_relative 'http_vpc/vpc_http'
+require_relative 'http/vpc_http'
 
-require_relative 'http_vpc/vpc_collection'
-require_relative 'http_vpc/vpc_instance'
-
+require_relative 'http/vpc_collection'
+require_relative 'http/vpc_instance'
