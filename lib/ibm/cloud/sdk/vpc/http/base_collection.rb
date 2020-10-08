7c9ff33efd397c3aab66203d11fbe9cf8d1fcdbc
Jared Moore <jaredm@ca.ibm.com>
2020-10-08 07:57:17 -0400

    Add VPC specific implementation of each_resource.

--- lib/ibm/cloud/sdk/vpc/http/base_collection.rb
+++ lib/ibm/cloud/sdk/vpc/http/base_collection.rb
@@ -28,6 +28,39 @@ module IBM
           def count
             fetch.json&.[](:total_count)
           end
+
+          private
+
+          # Return a wrapped instance if set.
+          # @param value [Hash] The hash returned from server.
+          def hash_instance(value)
+            return @instance.new(self, data: value, id_key: @instance_id) if @instance
+
+            value
+          end
+
+          # Create a generator that removes the need for pagination.
+          # @param url [String] Full URL to send to server.
+          # @return [Enumerator] Object to page through results.
+          # @yield [BaseInstance] An instance of the instance class.
+          # @yield [Hash] When no BaseInstance set.
+          def each_resource(url, &block)
+            return enum_for(:each_resource, url) unless block_given?
+            return unless url
+
+            response = get(path: url).json
+            resources = response.fetch(@array_key.to_sym)
+
+            resources&.each { |value| yield hash_instance(value) }
+            # VPC has a next key that holds the next URL.
+            return unless response.key?(:next)
+
+            # The :next data structure is a hash with a href member.
+            next_url = response.dig(:next, :href)
+            return unless next_url
+
+            each_resource(next_url, &block)
+          end
         end
       end
     end
