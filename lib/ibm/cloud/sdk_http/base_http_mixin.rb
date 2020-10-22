a86e5d2cda45a7f4443c3366e42d2a627276704a
Jared Moore <jaredm@ca.ibm.com>
2020-10-22 19:44:28 -0400

    Missed exposing payload_type

--- lib/ibm/cloud/sdk_http/base_http_mixin.rb
+++ lib/ibm/cloud/sdk_http/base_http_mixin.rb
@@ -10,30 +10,30 @@ module IBM
       module BaseHTTPMixin
         @connection = nil
 
-        def adhoc(method: 'get', path: nil, params: nil, payload: nil)
-          unchecked_response(method: method, path: path, params: params, payload: payload).raise_for_status!
+        def adhoc(method: 'get', path: nil, params: nil, payload: nil, payload_type: 'json')
+          unchecked_response(method: method, path: path, params: params, payload: payload, payload_type: payload_type).raise_for_status!
         end
 
-        def unchecked_response(method: 'get', path: nil, params: nil, payload: nil)
-          @connection.request(method.to_sym, url(path), metadata(params, payload))
+        def unchecked_response(method: 'get', path: nil, params: nil, payload: nil, payload_type: 'json')
+          @connection.request(method.to_sym, url(path), metadata(params, payload, payload_type))
         end
+
+        attr_reader :endpoint
 
         def get(path: nil, params: nil)
           adhoc(method: 'get', path: path, params: params)
         end
 
-        attr_reader :endpoint
-
-        def post(payload: nil, path: nil, params: nil)
-          adhoc(method: 'post', path: path, params: params, payload: payload)
+        def post(payload: nil, path: nil, params: nil, payload_type: 'json')
+          adhoc(method: 'post', path: path, params: params, payload: payload, payload_type: payload_type)
         end
 
-        def put(payload: nil, path: nil, params: nil)
-          adhoc(method: 'put', path: path, params: params, payload: payload)
+        def put(payload: nil, path: nil, params: nil, payload_type: 'json')
+          adhoc(method: 'put', path: path, params: params, payload: payload, payload_type: payload_type)
         end
 
-        def patch(payload: nil, path: nil, params: nil)
-          adhoc(method: 'patch', path: path, params: params, payload: payload)
+        def patch(payload: nil, path: nil, params: nil, payload_type: 'json')
+          adhoc(method: 'patch', path: path, params: params, payload: payload, payload_type: payload_type)
         end
 
         def delete(path: nil, params: nil)
